99ad046f7be7d4bc0b3ea3155c397ecb0db41dbc
sdkawata <sadaakikawata@gmail.com>
2020-02-22 09:03:40 +0900

    show codepoints

--- src/Index.tsx
+++ src/Index.tsx
@@ -1,10 +1,21 @@
-import React from 'react'
+import React, {useRef, useState} from 'react'
 import ReactDOM from 'react-dom'
 
 const Index: React.FC = () => {
+  const [text, setText] = useState("")
+  const textInput = useRef()
+  const textChanged = () => {
+    setText(textInput.current.value)
+  }
+  const codePoints = [...text]
   return (
     <div>
-      test
+      <input ref={textInput} onInput={textChanged}/>
+      {
+        codePoints.map((s, i) => {
+          return <div key={i}>{s} U+{s.codePointAt(0).toString(16)}</div>
+        })
+      }
     </div>
   )
 } 
