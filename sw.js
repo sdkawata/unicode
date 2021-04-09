c4ea0253b4f92292532d3e0deab758370944ddb5
Circle CI <>
2021-04-09 14:41:51 +0000

    [ci skip] release Fri Apr  9 14:41:51 UTC 2021

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME='unicode'
+
+self.addEventListener('install', (e) => {
+    e.waitUntil(
+      caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(['./ucd.json']))
+    )
+})
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    (async () => {
+      const cache = await caches.open(CACHE_NAME)
+      const response = await cache.match(e.request)
+      console.log(e.request)
+      if (response) {
+        console.log('found')
+        return response
+      }
+      return await fetch(e.request)
+    })()
+  )
+})
